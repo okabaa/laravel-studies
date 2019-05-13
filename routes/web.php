ec319e9f751a9f9ccb8898534a6a9f70d993a322
Oğuzhan KABA <oguzhan-kaba@outlook.com>
2019-05-14 02:40:55 +0300

    Laravel Ders 05

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,15 @@ Route::get('/deneme', 'HomeController@get_deneme');
 Route::get('/form', 'HomeController@get_form');
 Route::post('/form', 'HomeController@post_form');
 //Route::get('/deneme/{isim}','HomeController@get_deneme_isim');
-Route::group(['prefix'=>'deneme'], function(){
+
+Route::group(['prefix'=>'admin','middleware'=>'admin'], function(){
     Route::get('/forum', 'HomeController@get_deneme');
     Route::get('/blog', 'HomeController@get_deneme');
-    Route::get('/haberler', 'HomeController@get_deneme');
+    Route::group(['prefix'=>'haber','middleware'=>'yazar'], function(){
+        Route::get('/haber-ekle', 'HomeController@get_deneme');
+        Route::get('/haberler', 'HomeController@get_deneme');
+    });
+
     Route::get('/galeri', 'HomeController@get_deneme');
 });
 
